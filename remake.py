2795a3bba4023e49bc583c2587829aa4d79d17ad
wish1 <aswq22013@gmail.com>
2019-02-07 02:22:42 +0300

    Update remake.py

--- remake.py
+++ remake.py
@@ -1,8 +1,12 @@
-import sys
++"\n"import sys
 
 smth = open(sys.argv[1],"r")
 input = open(TMP.txt,"r")
 output = open(MasterList.txt,"w")
-for line in input:
-  line = line.split(" ")
-  output.write(
+while input:
+  line = smth.readline().split()
+  paths = input.readline().split()
+  if len(line) == 7:
+    output.write(line[0] + ";" + line[1] + ";" + line[2] + ";" + line[3] + ";" + paths[0] + ";" + line[4] + ";" + paths[1] + ";" + paths[2] + ";" + paths[3] + ";" + paths[4] + ";" + line[5] + ";" + line[6] + ";" + paths[5] +"\n")
+   else:
+    output.write(line[0] + ";" +  line[1] + ";" +  line[2] + ";" + line[3] + ";" + paths[0] + ";" + line[4] + ";" + paths[1] + ";" + paths[2] + ";" + paths[3] + ";" + paths[4] + "\n")
