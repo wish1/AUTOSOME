2b0dda0dc8402482d68d07f11fd1869c8c929ba8
wish1 <aswq22013@gmail.com>
2019-02-07 01:46:55 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -7,14 +7,13 @@ REF=$1
 VCF=$2
 Num=$3
 n=0
-ALIGNCTRL=false
-
+ALIGNCTRL="1"
 while read LINE; do
 	if $n==$Num;then
 		IFS='	'
 		read -ra ADDR <<< $LINE
 		if [ ${ADDR[2]}=="Homo sapiens" ]; then
-			if ${#arr[*]}==11;then
+			if [ ${#ADDR[*]}==13 ];then
 				EXP=${ADDR[0]}
 				echo "making $EXP with ctrl"
 				TF=${ADDR[1]}
@@ -25,8 +24,9 @@ while read LINE; do
 				SISSRS=${ADDR[7]}
 				CPICS=${ADDR[8]}
 				GEM=${ADDR[9]}
-				ALIGNCTRL=${ADDR[10]}
-				CTRLPATH=${ADDR[11]}
+				EXPCTRL=${ADDR[10]}
+				ALIGNCTRL=${ADDR[11]}
+				CTRLPATH=${ADDR[12]}
 			else:
 				EXP=${ADDR[0]}
 				echo "making $EXP without ctrl"
@@ -41,100 +41,111 @@ while read LINE; do
 			fi
 		fi
 	
-		if ! [ -d /home/Abramov/Alignments/"$TF" ]; then
+		if ! [ -d /home/Abramov/Alignments/EXP/"$TF" ]; then
 			mkdir /home/Abramov/Alignments/"$TF"
 			if [ $? != 0 ]; then
-				echo "Failed to make dir"
+				echo "Failed to make dir $TF"
 	    			exit 1
 			fi
 		fi
 	
-		mkdir /home/Abramov/Alignments/"$TF/$EXP"
+		mkdir /home/Abramov/Alignments/EXP/"$TF/$EXP"
 		if [ $? != 0 ]; then
-			echo "Failed to make dir"
+			echo "Failed to make dir $EXP"
 	    		exit 1
 		fi
 		
-		scp -P 1235  autosome@127.0.0.1:$ALIGNPATH /home/Abramov/Alignments/"$TF/$EXP/$ALIGNEXP.bam"
+		if ! [ -d /home/Abramov/Alignments/"$TF/$EXPCTRL" ]; then
+			mkdir /home/Abramov/Alignments/CTRL/"$TF/$EXPCTRL"
+			if [ $? != 0 ]; then
+				echo "Failed to make dir $EXPCTRL"
+	    			exit 1
+			fi
+		else
+			ALIGNCTRL="1"
+		fi
+		
+		scp -P 1235  autosome@127.0.0.1:$ALIGNPATH /home/Abramov/Alignments/EXP/"$TF/$EXP/$ALIGNEXP.bam"
 		if [ $? != 0 ]; then
 			echo "Failed to download ALIGNEXP $EXP"
 	    		exit 1
 		fi
 		
-		scp -P 1235  autosome@127.0.0.1:$MACS /home/Abramov/Alignments/"$TF/$EXP/${PEAKS}_macs.interval"
+		scp -P 1235  autosome@127.0.0.1:$MACS /home/Abramov/Alignments/EXP/"$TF/$EXP/${PEAKS}_macs.interval"
 		if [ $? != 0 ]; then
 			echo "Failed to download MACS $EXP"
 	    		exit 1
 		fi
 		
-		scp -P 1235  autosome@127.0.0.1:$SISSRS /home/Abramov/Alignments/"$TF/$EXP/${PEAKS}_sissrs.interval"
+		scp -P 1235  autosome@127.0.0.1:$SISSRS /home/Abramov/Alignments/EXP/"$TF/$EXP/${PEAKS}_sissrs.interval"
 		if [ $? != 0 ]; then
 			echo "Failed to download SISSRS $EXP"
 	    		exit 1
 		fi
 		
-		scp -P 1235  autosome@127.0.0.1:$CPICS /home/Abramov/Alignments/"$TF/$EXP/${PEAKS}_cpics.interval"
+		scp -P 1235  autosome@127.0.0.1:$CPICS /home/Abramov/Alignments/EXP/"$TF/$EXP/${PEAKS}_cpics.interval"
 		if [ $? != 0 ]; then
 			echo "Failed to download CPICS $EXP"
 	    		exit 1
 		fi
 		
-		scp -P 1235  autosome@127.0.0.1:$GEM /home/Abramov/Alignments/"$TF/$EXP/${PEAKS}_gem.interval"
+		scp -P 1235  autosome@127.0.0.1:$GEM /home/Abramov/Alignments/EXP/"$TF/$EXP/${PEAKS}_gem.interval"
 		if [ $? != 0 ]; then
 			echo "Failed to download GEM $EXP"
 	    		exit 1
 		fi
 		
-		if $ALIGNCTRL; then
-			scp -P 1235  autosome@127.0.0.1:$CTRLPATH /home/Abramov/Alignments/"$TF/$EXP/$ALIGNCTRL.bam"
+		if [ $ALIGNCTRL!="1" ]; then
+			scp -P 1235  autosome@127.0.0.1:$CTRLPATH /home/Abramov/Alignments/CTRL/"$TF/$EXPCTRL/$ALIGNCTRL.bam"
 			if [ $? != 0 ]; then
 				echo "Failed to download ALIGNCTRL $EXP"
 	    		exit 1
 			fi
 		fi
 		
-		#fix 2 versions of SNPcalling
-		if $ALIGNCTRL; then
+		
+		if [ $ALIGNCTRL!="1" ]; then
 			bash SNPcalling.sh -Ref $REF \
-				-Exp /home/Abramov/Alignments/"$TF/$EXP/$ALIGNEXP.bam" \
-				-Ctrl /home/Abramov/Alignments/"$TF/$EXP/$ALIGNCTRL.bam" \
+				-Exp /home/Abramov/Alignments/EXP/"$TF/$EXP/$ALIGNEXP.bam" \
+				-Ctrl /home/Abramov/Alignments/CTRL/"$TF/$EXP/$ALIGNCTRL.bam" \
 				-WGE \
 				-WGC \
 				-VCF $VCF \
-				-Out /home/Abramov/Alignments/"$TF/$EXP" \
-				-macs /home/Abramov/Alignments/"$TF/$EXP/${PEAKS}_macs.interval" \
-				-sissrs /home/Abramov/Alignments/"$TF/$EXP/${PEAKS}_sissrs.interval" \
-				-gem /home/Abramov/Alignments/"$TF/$EXP/${PEAKS}_gem.interval" \
-				-cpics /home/Abramov/Alignments/"$TF/$EXP/${PEAKS}_cpics.interval"
+				-Out /home/Abramov/Alignments/EXP/"$TF/$EXP" \
+				-macs /home/Abramov/Alignments/EXP/"$TF/$EXP/${PEAKS}_macs.interval" \
+				-sissrs /home/Abramov/Alignments/EXP/"$TF/$EXP/${PEAKS}_sissrs.interval" \
+				-gem /home/Abramov/Alignments/EXP/"$TF/$EXP/${PEAKS}_gem.interval" \
+				-cpics /home/Abramov/Alignments/EXP/"$TF/$EXP/${PEAKS}_cpics.interval"
 			if [ $? != 0 ]; then
 				echo "Failed SNPcalling $EXP"
 	    			exit 1
 			fi
+			rm /home/Abramov/Alignments/CTRL/"$TF/$EXPCTRL/$ALIGNCTRL.bam"
 		else
 			bash SNPcalling.sh -Ref $REF \
-				-Exp /home/Abramov/Alignments/"$TF/$EXP/$ALIGNEXP.bam" \
+				-Exp /home/Abramov/Alignments/EXP/"$TF/$EXP/$ALIGNEXP.bam" \
 				-WGE \
 				-VCF $VCF \
-				-Out /home/Abramov/Alignments/"$TF/$EXP" \
-				-macs /home/Abramov/Alignments/"$TF/$EXP/${PEAKS}_macs.interval" \
-				-sissrs /home/Abramov/Alignments/"$TF/$EXP/${PEAKS}_sissrs.interval" \
-				-gem /home/Abramov/Alignments/"$TF/$EXP/${PEAKS}_gem.interval" \
-				-cpics /home/Abramov/Alignments/"$TF/$EXP/${PEAKS}_cpics.interval"
+				-Out /home/Abramov/Alignments/EXP/"$TF/$EXP" \
+				-macs /home/Abramov/Alignments/EXP/"$TF/$EXP/${PEAKS}_macs.interval" \
+				-sissrs /home/Abramov/Alignments/EXP/"$TF/$EXP/${PEAKS}_sissrs.interval" \
+				-gem /home/Abramov/Alignments/EXP/"$TF/$EXP/${PEAKS}_gem.interval" \
+				-cpics /home/Abramov/Alignments/EXP/"$TF/$EXP/${PEAKS}_cpics.interval"
 			if [ $? != 0 ]; then
 				echo "Failed SNPcalling $EXP"
 	    			exit 1
 			fi
 			
-			rm /home/Abramov/Alignments/"$TF/$EXP/$ALIGNCTRL.bam"
+			
 		
 		fi
 		
 		
-		rm /home/Abramov/Alignments/"$TF/$EXP/$ALIGNEXP.bam"	
-		rm /home/Abramov/Alignments/"$TF/$EXP/${PEAKS}_macs.interval"
-		rm /home/Abramov/Alignments/"$TF/$EXP/${PEAKS}_sissrs.interval"
-		rm /home/Abramov/Alignments/"$TF/$EXP/${PEAKS}_gem.interval"
-		rm /home/Abramov/Alignments/"$TF/$EXP/${PEAKS}_cpics.interval"
+		rm /home/Abramov/Alignments/EXP/"$TF/$EXP/$ALIGNEXP.bam"	
+		rm /home/Abramov/Alignments/EXP/"$TF/$EXP/${PEAKS}_macs.interval"
+		rm /home/Abramov/Alignments/EXP/"$TF/$EXP/${PEAKS}_sissrs.interval"
+		rm /home/Abramov/Alignments/EXP/"$TF/$EXP/${PEAKS}_gem.interval"
+		rm /home/Abramov/Alignments/EXP/"$TF/$EXP/${PEAKS}_cpics.interval"
 	fi
 		n=$n+1
 done < ./MasterList.txt
